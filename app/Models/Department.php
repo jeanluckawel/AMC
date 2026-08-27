3e8c690439c8b9c1d0bb56d9ab8604c8b1fc8474
Jean Luc Kawel <20ka208@esisalama.org>
2026-08-27 14:20:10 +0200

    Introduce `JobTitle` and `Department` models with migrations; refactor relationships and add auto-matricule generation for `Employee`.

---
+++ app/Models/Department.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Department extends Model
+{
+    //
+
+    use HasUlids, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'code',
+        'description',
+    ];
+
+    public function employees(): HasMany
+    {
+        return $this->hasMany(Employee::class);
+    }
+
+    public function assignments(): HasMany
+    {
+        return $this->hasMany(EmployeeAssignment::class);
+    }
+}
